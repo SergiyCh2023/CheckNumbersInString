1f0e241d3acd29a448cdaa15a6e34e4dcd155eee
SergiyCh2023 <alien13579@ukr.net>
2024-08-06 13:16:11 +0300

    The second solution

--- src/main/java/org/example/Solution2.java
+++ src/main/java/org/example/Solution2.java
@@ -1,13 +1,11 @@
 package org.example;
 
-public class Solution {
+public class Solution2 {
 
     public boolean checkingIfContainsOnlyNumbers(String s) {
         if (s.length() == 0) throw new NonSymbolsException();
-
-        return !s.matches("\\d+");
-
-
+        return s.chars().allMatch(e -> Character.isDigit(e));
     }
+
 
 }
