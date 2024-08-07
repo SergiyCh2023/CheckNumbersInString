f620a5e1f2400b46a22c2eddcd12b64a304d7601
SergiyCh2023 <alien13579@ukr.net>
2024-08-07 18:44:02 +0300

    the fourth commit

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -5,7 +5,7 @@ public class Solution {
     public boolean checkingIfContainsOnlyNumbers(String s) {
         if (s.length() == 0) throw new NonSymbolsException();
 
-        return !s.matches("\\d+");
+        return s.matches("\\d+");
 
 
     }
